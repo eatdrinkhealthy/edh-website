320f131ee7ee2b3fdf1aec77a9dc7612f20c0d23
stevenjmarsh <sjm.atl@gmail.com>
2017-01-26 14:32:47 -0500

    adding snapshot test for privacy-policy

--- imports/ui/pages/tests/PrivacyPolicy.jest.js
+++ imports/ui/pages/tests/PrivacyPolicy.jest.js
@@ -2,14 +2,21 @@
 /* eslint-disable func-names, prefer-arrow-callback, no-unused-expressions */
 
 import React from "react";
+import renderer from "react-test-renderer";
 import {
     shallow,
 } from "enzyme";
+
 import PrivacyPolicy from "../PrivacyPolicy";
 
 describe("<PrivacyPolicy />", function () {
+  it("renders correctly", function () {
+    const tree = renderer.create(<PrivacyPolicy />).toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+
   it("renders Privacy Policy header and 5 sections", function () {
-    const wrapper = shallow(<PrivacyPolicy/>);
+    const wrapper = shallow(<PrivacyPolicy />);
     expect(wrapper.type()).toEqual("div");
   });
 });

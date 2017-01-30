ae89716f068e503abd82a197d962508091a2899e
stevenjmarsh <sjm.atl@gmail.com>
2017-01-30 15:58:39 -0500

    adding SplashPage (page / component) and removing hello placeholder component

--- imports/config/Routes.js
+++ imports/config/Routes.js
@@ -1,12 +1,12 @@
 import React from "react";
 import { Router, Route, browserHistory } from "react-router";
-import Hello from "../ui/components/Hello";
+import SplashPage from "../ui/pages/SplashPage";
 import PrivacyPolicy from "../ui/pages/PrivacyPolicy";
 import Support from "../ui/pages/Support";
 
 const Routes = (
   <Router history={browserHistory}>
-    <Route path="/" component={Hello} />
+    <Route path="/" component={SplashPage} />
     <Route path="/privacy-policy" component={PrivacyPolicy} />
     <Route path="/support" component={Support} />
   </Router>
--- imports/ui/pages/__stories__/Pages.stories.js
+++ imports/ui/pages/__stories__/Pages.stories.js
@@ -2,9 +2,12 @@ import React from "react";
 import {
   storiesOf,
 } from "@kadira/storybook";
+import SplashPage from "../SplashPage";
 import Support from "../Support";
 import PrivacyPolicy from "../PrivacyPolicy";
 
+
 storiesOf("Pages", module)
+  .add("splash page", () => (<SplashPage />))
   .add("support", () => (<Support />))
   .add("privacy policy", () => (<PrivacyPolicy />));

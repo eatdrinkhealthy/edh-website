3d834c8670236eaa1b01aa84356e8e88c9b9981f
stevenjmarsh <sjm.atl@gmail.com>
2017-02-03 14:34:27 -0500

    removing test class attribute from real components

--- imports/ui/components/Logo.js
+++ imports/ui/components/Logo.js
@@ -2,7 +2,7 @@ import React from "react";
 import { logoStyle } from "../styles";
 
 const Logo = () => (
-  <div className={`${logoStyle.className} anotherClass`} />
+  <div className={`${logoStyle.className}`} />
 );
 
 Logo.style = logoStyle;
--- imports/ui/components/Pitch.js
+++ imports/ui/components/Pitch.js
@@ -2,7 +2,7 @@ import React from "react";
 import { pitchStyle } from "../styles";
 
 const Pitch = () => (
-  <div className={`${pitchStyle.className} anotherClass`}>
+  <div className={`${pitchStyle.className}`}>
     A platform to find, share, and discuss healthy places to shop and eat.
   </div>
 );

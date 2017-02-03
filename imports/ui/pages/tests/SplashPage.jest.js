fa1c9408ededab6ebd6b937e13e9a6142f97ffcf
stevenjmarsh <sjm.atl@gmail.com>
2017-02-03 11:20:35 -0500

    removing some tests used for practice with ReactTestUtils, react-dom render, static render, ...

--- imports/ui/pages/tests/SplashPage.jest.js
+++ imports/ui/pages/tests/SplashPage.jest.js
@@ -1,69 +1,28 @@
 /* eslint-env jest */
 /* eslint-disable func-names, prefer-arrow-callback, no-unused-expressions */
+/* eslint-disable import/no-extraneous-dependencies */
 
 import React from "react";
 import {
-  render,
-  findDOMNode,
-  unmountComponentAtNode,
-} from "react-dom";
-import ReactTestUtils from "react-addons-test-utils";
-import {
   shallow,
-  mount,
-  render as staticRender,
 } from "enzyme";
 import renderer from "react-test-renderer";
 import SplashPage from "../SplashPage";
-import { comingSoonStyle } from "../../styles";
 import { getComponentClasses } from "../../../utils/testUtils";
 
 describe("<SplashPage />", function () {
-  it("render matches snapshot", function () {
+  it("matches render snapshot", function () {
     const tree = renderer.create(<SplashPage />).toJSON();
     expect(tree).toMatchSnapshot();
   });
 
-  it("css matches snapshot", function () {
+  it.skip("matches css snapshot", function () {
+    // TODO complete getting classes to snapshot
+    const classList = getComponentClasses(<SplashPage />);
+    expect(classList.length).toBeGreaterThan(0);
+
     expect(shallow(<SplashPage />).hasClass(SplashPage.style.className)).toBe(true);
     expect(JSON.stringify(SplashPage.style, null, 2)).toMatchSnapshot();
-  });
-
-  it.skip("document practice", function () {
-    document.body.innerHTML =
-      `<div>` +
-      `<div id="renderSpot" />` +
-      `</div>`;
-
-    render(<h1 style={{ color: 'blue' }} className={SplashPage.style.className}>Try
-      me.</h1>, document.getElementById("renderSpot"));
-    console.log(document.body.innerHTML);
-  });
-
-  it.skip("mount practice", function () {
-    // const wrapper = mount(<h1 style={{color: "purple"}} myProp1="my prop!">This test stuff.</h1>);
-    const wrapper = mount(<h1 className={comingSoonStyle.className}>This test stuff.</h1>);
-    // const domNode = findDOMNode(wrapper.node);
-    console.log("document body:", document.head);
-  });
-
-  it("lists component and children classes", function () {
-    const classList = getComponentClasses(<SplashPage className="onemore" prop1="testbadprop" />);
-    console.log("class list:", classList);
-  });
-
-  it.skip("practice with static render (cheerio wrapper)", function () {
-    const wrapper = staticRender(<SplashPage />);
-    console.log("static render:", wrapper['0'].children);
-  });
-
-  it.skip("test utils practice", function () {
-    // const wrapper = ReactTestUtils.renderIntoDocument(<h1 className={comingSoonStyle.className}>This test stuff.</h1>);
-    const wrapper = ReactTestUtils.renderIntoDocument(<div><h1 className="Bortz">This test stuff.</h1><h2
-      className="Neil">Neil is here.</h2></div>);
-    // const wrapper = ReactTestUtils.renderIntoDocument(<SplashPage />);
-    console.log("rendered:", wrapper);
-    console.log("document head:", document.head);
   });
 
   it("contains sections company name, pitch, coming soon, logo, preview ", function () {

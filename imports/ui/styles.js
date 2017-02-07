a477d1556ebfa75f537a144a43672e957c862a91
stevenjmarsh <sjm.atl@gmail.com>
2017-02-07 12:59:57 -0500

    adding comment regarding pattern for component style objects

--- imports/ui/styles.js
+++ imports/ui/styles.js
@@ -1,5 +1,19 @@
 import { css } from "glamor";
 
+//
+// Component Style Pattern (use with glamor)
+//
+// const componentRules = {
+//    rule1: "value",
+//      ...
+//    ruleN: "value",
+// };
+// export const componentStyle = {
+//    rules: componentRules,
+//    className: `${css(componentRules)}`,
+// };
+//
+
 const gradientBackgroundRules = {
   background: [
     "#048ec5",

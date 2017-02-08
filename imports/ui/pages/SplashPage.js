ceeff0c9e0ce47747cde0e08f3b7456713830a83
stevenjmarsh <sjm.atl@gmail.com>
2017-02-07 19:05:38 -0500

    removing unused import, clearing an eslint error

--- imports/ui/pages/SplashPage.js
+++ imports/ui/pages/SplashPage.js
@@ -1,5 +1,4 @@
 import React from "react";
-import { defaultPageStyle } from "../styles";
 import CompanyName from "../components/CompanyName";
 import Pitch from "../components/Pitch";
 import ComingSoon from "../components/ComingSoon";

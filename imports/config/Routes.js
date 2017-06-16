84da8cecd4b62ff11a3ac3af40b21e07eb4a3278
stevenjmarsh <sjm.atl@gmail.com>
2017-06-16 17:18:38 -0400

    initial routing with React Router v4 (root only works)

--- imports/config/Routes.js
+++ imports/config/Routes.js
@@ -1,15 +1,17 @@
 import React from "react";
-import { Router, Route, browserHistory } from "react-router";
+import { BrowserRouter, Route } from "react-router-dom";
 import SplashPage from "../ui/pages/SplashPage";
 import PrivacyPolicy from "../ui/pages/PrivacyPolicy";
 import Support from "../ui/pages/Support";
 
 const Routes = (
-  <Router history={browserHistory}>
-    <Route path="/" component={SplashPage} />
-    <Route path="/privacy-policy" component={PrivacyPolicy} />
-    <Route path="/support" component={Support} />
-  </Router>
+  <BrowserRouter>
+    <div>
+      <Route path="/" component={SplashPage} />
+      <Route path="/privacy-policy" component={PrivacyPolicy} />
+      <Route path="/support" component={Support} />
+    </div>
+  </BrowserRouter>
 );
 
 export default Routes;

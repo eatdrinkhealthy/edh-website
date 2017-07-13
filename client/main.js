58a1ba12aa2b89ec386fefa83166d57f9c9b4649
stevenjmarsh <sjm.atl@gmail.com>
2017-07-13 13:25:35 -0400

    moving 'routes' to App component

--- client/main.js
+++ client/main.js
@@ -1,10 +1,10 @@
 import { Meteor } from "meteor/meteor";
+import React from "react";
 import { render } from "react-dom";
+import App from "../imports/ui/containers/App";
 
 /* global document */
 
-import Routes from "../imports/config/Routes";
-
 Meteor.startup(() => {
-  render(Routes, document.getElementById("render-target"));
+  render(App, document.getElementById("render-target"));
 });

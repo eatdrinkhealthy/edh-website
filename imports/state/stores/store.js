2a969b9ecf05fe8d8e77568da22a0b5461ba7d1c
stevenjmarsh <sjm.atl@gmail.com>
2018-01-09 18:34:26 -0500

    removing unnecessary / empty defaultState from createStore

--- imports/state/stores/store.js
+++ imports/state/stores/store.js
@@ -1,8 +1,6 @@
 import { createStore } from "redux";
 import appReducer from "../reducers";
 
-const defaultState = {};
-
-const store = createStore(appReducer, defaultState);
+const store = createStore(appReducer);
 
 export default store;

273fe7c8afc45261be627c9b35449d093a2c3493
stevenjmarsh <sjm.atl@gmail.com>
2018-01-09 16:39:14 -0500

    adding ContactUsForm placeholder

---
+++ imports/ui/components/tests/__stories__/ReduxForms.stories.jsx
@@ -0,0 +1,6 @@
+import React from "react";
+import { storiesOf } from "@storybook/react";
+import ContactUsForm from "../../ContactUsForm";
+
+storiesOf("Redux Forms", module)
+  .add("Contact Us Form", () => <ContactUsForm />);

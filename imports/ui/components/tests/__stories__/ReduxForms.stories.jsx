36598611c93d485b4ff7753d43fc488b57797a83
stevenjmarsh <sjm.atl@gmail.com>
2018-01-09 18:29:07 -0500

    formatting code, redux forms storybook

--- imports/ui/components/tests/__stories__/ReduxForms.stories.jsx
+++ imports/ui/components/tests/__stories__/ReduxForms.stories.jsx
@@ -2,5 +2,4 @@ import React from "react";
 import { storiesOf } from "@storybook/react";
 import ContactUsForm from "../../ContactUsForm";
 
-storiesOf("Redux Forms", module)
-  .add("Contact Us Form", () => <ContactUsForm />);
+storiesOf("Redux Forms", module).add("Contact Us Form", () => <ContactUsForm />);

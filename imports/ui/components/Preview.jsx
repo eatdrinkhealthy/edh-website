e7aefbac9f6a09e688f7fa6b155e130b6d3bca82
stevenjmarsh <sjm.atl@gmail.com>
2018-01-09 15:04:25 -0500

    formatting code using prettier

--- imports/ui/components/Preview.jsx
+++ imports/ui/components/Preview.jsx
@@ -1,8 +1,5 @@
 import React from "react";
-import {
-  appLinksHeaderStyle,
-  appLinksButtonsStyle,
-} from "../styles";
+import { appLinksHeaderStyle, appLinksButtonsStyle } from "../styles";
 import LinkButton from "./LinkButton";
 
 const Preview = () => (
--- imports/ui/components/tests/LinkButton.jest.jsx
+++ imports/ui/components/tests/LinkButton.jest.jsx
@@ -3,20 +3,22 @@
 import React from "react";
 import renderer from "react-test-renderer";
 import LinkButton from "../LinkButton";
-import {
-  formattedJSON,
-  getAllComponentStyle,
-} from "../../../utils/tests/testUtils";
+import { formattedJSON, getAllComponentStyle } from "../../../utils/tests/testUtils";
 import * as styles from "../../../ui/styles";
 
 describe("<LinkButton />", function () {
   it("matches render snapshot", function () {
-    const tree = renderer.create(<LinkButton text="some button text" url="some link" />).toJSON();
+    const tree = renderer
+      .create(<LinkButton text="some button text" url="some link" />)
+      .toJSON();
     expect(tree).toMatchSnapshot();
   });
 
   it("matches style / css snapshot", function () {
-    const allStyle = getAllComponentStyle(<LinkButton text="button text" url="http..." />, styles);
+    const allStyle = getAllComponentStyle(
+      <LinkButton text="button text" url="http..." />,
+      styles,
+    );
     expect(formattedJSON(allStyle)).toMatchSnapshot();
   });
 });

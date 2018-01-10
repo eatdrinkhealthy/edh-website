a8aa222e5d20de354db64071b3ffe7ed27266d86
stevenjmarsh <sjm.atl@gmail.com>
2018-01-10 13:27:28 -0500

    renaming ContactUsForm to ContactForm

--- imports/ui/components/tests/__stories__/ReduxForms.stories.jsx
+++ imports/ui/components/tests/__stories__/ReduxForms.stories.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 import { storiesOf } from "@storybook/react";
 import Provider from "./Provider";
-import ContactUsForm from "../../ContactUsForm";
+import ContactForm from "../../ContactForm";
 
 storiesOf("Redux Forms", module)
   .addDecorator(story => <Provider story={story()} />)
-  .add("Contact Us Form", () => <ContactUsForm />);
+  .add("Contact Form", () => <ContactForm />);

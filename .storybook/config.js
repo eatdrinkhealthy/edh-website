9de9298789d9cbdb2a35c0b596b9c1b65868d937
stevenjmarsh <sjm.atl@gmail.com>
2018-01-18 14:50:00 -0500

    removing Header (new ui) from storybook

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,7 +1,5 @@
 import { configure } from "@storybook/react";
 
-import "!style-loader!css-loader!../client/app.css";
-
 const req = require.context("../imports/ui", true, /__stories__\/.*.stories.jsx?/);
 
 function loadStories () {
--- imports/ui/pages/tests/__stories__/PageSections.stories.jsx
+++ imports/ui/pages/tests/__stories__/PageSections.stories.jsx
@@ -1,8 +1,5 @@
 import React from "react";
 import { storiesOf } from "@storybook/react";
 import Preview from "../../../components/Preview";
-import Header from "../../../components/Header";
 
-storiesOf("Page Sections", module)
-  .add("Header", () => <Header />)
-  .add("Preview", () => <Preview />);
+storiesOf("Page Sections", module).add("Preview", () => <Preview />);

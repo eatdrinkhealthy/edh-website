152e86d9b9af0ac9dd05f8cb7ee25f2ac5c490a7
stevenjmarsh <sjm.atl@gmail.com>
2018-01-30 15:55:20 -0500

    moving propTypes and defaultProps to class properties of AlertMessage

--- imports/ui/components/AlertMessage.jsx
+++ imports/ui/components/AlertMessage.jsx
@@ -1,25 +1,24 @@
-import "react-s-alert/dist/s-alert-default.css";
 import Alert from "react-s-alert";
 import PropTypes from "prop-types";
 
 // note, css required for this package is imported in 'css-packages.css'
 
-class AlertMessage extends Alert {}
+class AlertMessage extends Alert {
+  static propTypes = {
+    position: PropTypes.string,
+    offset: PropTypes.number,
+    stack: PropTypes.oneOfType([
+      PropTypes.bool, // boolean | { limit: number },
+      PropTypes.object,
+    ]),
+  };
 
-AlertMessage.propTypes = {
-  position: PropTypes.string,
-  offset: PropTypes.number,
-  stack: PropTypes.oneOfType([
-    PropTypes.bool, // boolean | { limit: number },
-    PropTypes.object,
-  ]),
-};
-
-// customizing default props (slightly different than Alert)
-AlertMessage.defaultProps = {
-  position: "top",
-  offset: 0,
-  stack: { limit: 5 },
-};
+  // customizing default props (slightly different than Alert)
+  static defaultProps = {
+    position: "top",
+    offset: 0,
+    stack: { limit: 5 },
+  };
+}
 
 export default AlertMessage;

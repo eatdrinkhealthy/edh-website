a82d52c7cc8c159222b9c1943284197d873cb2e2
stevenjmarsh <sjm.atl@gmail.com>
2018-01-30 11:22:46 -0500

    adding Provider (redux) around SplashPage to fix broken test

--- imports/ui/pages/tests/SplashPage.jest.jsx
+++ imports/ui/pages/tests/SplashPage.jest.jsx
@@ -1,18 +1,25 @@
 /* eslint-env jest */
 /* eslint-disable func-names, prefer-arrow-callback, no-unused-expressions */
 import React from "react";
+import { createStore } from "redux";
+import { Provider } from "react-redux";
 import { shallow } from "enzyme";
 import renderer from "react-test-renderer";
 import { MemoryRouter } from "react-router-dom";
 import SplashPage from "../SplashPage";
+import appReducer from "../../../state/reducers";
 
 describe("<SplashPage />", function () {
+  const testStore = createStore(appReducer);
+
   it("matches render snapshot", function () {
     const tree = renderer
       .create(
-        <MemoryRouter>
-          <SplashPage />
-        </MemoryRouter>,
+        <Provider store={testStore}>
+          <MemoryRouter>
+            <SplashPage />
+          </MemoryRouter>
+        </Provider>,
       )
       .toJSON();
     expect(tree).toMatchSnapshot();

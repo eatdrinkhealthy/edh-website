da055999c90eb9bea9f90edce0179f734152bc5c
stevenjmarsh <sjm.atl@gmail.com>
2018-01-31 12:11:16 -0500

    adding validation to ContactForm fields name and email

--- imports/ui/components/ContactForm.jsx
+++ imports/ui/components/ContactForm.jsx
@@ -2,23 +2,37 @@ import React from "react";
 import PropTypes from "prop-types";
 import { Field, reduxForm } from "redux-form";
 
-const ValidationError = ({ children, visible }) => {
-  const classes = `form-error ${visible ? "is-visible" : ""}`;
+const required = value => (value ? undefined : "Required field.");
+const emailFormat = value =>
+  value && !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(value)
+    ? "Invalid email address."
+    : undefined;
 
-  return <span className={classes}>{children}</span>;
+/* eslint-disable jsx-a11y/label-has-for, jsx-a11y/no-autofocus */
+
+const renderInput = ({ input, label, type, autoFocus, meta: { touched, error } }) => (
+  <div>
+    <label htmlFor={input.name}>{label}</label>
+    <input {...input} component="input" type={type} autoFocus={autoFocus} />
+    {touched && error && <span className="form-error is-visible">{error}</span>}
+  </div>
+);
+
+renderInput.propTypes = {
+  input: PropTypes.shape({
+    name: PropTypes.string.isRequired,
+    value: PropTypes.string.isRequired,
+  }).isRequired, // eslint-disable-line react/forbid-prop-types
+  label: PropTypes.string.isRequired,
+  type: PropTypes.string.isRequired,
+  autoFocus: PropTypes.bool,
+  meta: PropTypes.shape({
+    touched: PropTypes.bool.isRequired,
+    error: PropTypes.stringisRequired,
+  }).isRequired,
 };
 
-ValidationError.propTypes = {
-  children: PropTypes.node,
-  visible: PropTypes.bool,
-};
-
-ValidationError.defaultProps = {
-  children: "",
-  visible: false,
-};
-
-/* eslint-disable jsx-a11y/label-has-for */
+renderInput.defaultProps = { autoFocus: false };
 
 const ContactFormComponent = props => {
   const { handleSubmit } = props;
@@ -27,22 +41,26 @@ const ContactFormComponent = props => {
     <div>
       <h2>Contact Us</h2>
       <form onSubmit={handleSubmit}>
-        <div>
-          <label htmlFor="contactName">Name </label>
-          <Field name="contactName" component="input" type="text" autoFocus />
-          <ValidationError visible>validation error.</ValidationError>
-        </div>
+        <Field
+          name="contactName"
+          label="Name"
+          component={renderInput}
+          type="text"
+          validate={required}
+          autoFocus
+        />
 
-        <div>
-          <label htmlFor="contactEmail">Email </label>
-          <Field name="contactEmail" component="input" type="email" />
-          <ValidationError>validation error.</ValidationError>
-        </div>
+        <Field
+          name="contactEmail"
+          label="Email"
+          component={renderInput}
+          type="email"
+          validate={[required, emailFormat]}
+        />
 
         <div>
           <label htmlFor="contactMessage">Message </label>
           <Field name="contactMessage" component="textarea" rows={10} cols={50} />
-          <ValidationError>validation error.</ValidationError>
         </div>
 
         <button id="contactSubmit" type="submit" className="button button--submit">

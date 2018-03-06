53290bcd54f781a37e6c4c018cc57f49638fbb88
stevenjmarsh <sjm.atl@gmail.com>
2018-03-06 18:03:43 -0500

    removing SplashPage from storybook pages

--- imports/ui/pages/tests/__stories__/Pages.stories.jsx
+++ imports/ui/pages/tests/__stories__/Pages.stories.jsx
@@ -1,10 +1,8 @@
 import React from "react";
 import { MemoryRouter } from "react-router-dom";
 import { storiesOf } from "@storybook/react";
-import SplashPage from "../../SplashPage";
 import PrivacyPolicy from "../../PrivacyPolicy";
 
 storiesOf("Pages", module)
   .addDecorator(story => <MemoryRouter>{story()}</MemoryRouter>)
-  .add("splash page", () => <SplashPage />)
   .add("privacy policy", () => <PrivacyPolicy />);
